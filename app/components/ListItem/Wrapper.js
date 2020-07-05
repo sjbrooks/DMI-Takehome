821ec2462f9b35987af352e7a2e85cd1fb7b6c3e
Sarah <sjbrooks305@gmail.com>
2020-07-05 14:31:27 -0700

    Removed unused components and replaced default images

--- app/components/ListItem/Wrapper.js
+++ app/components/ListItem/Wrapper.js
@@ -2,10 +2,11 @@ import styled from 'styled-components';
 
 const Wrapper = styled.li`
   width: 100%;
-  height: 3em;
+  min-height: 3em;
   display: flex;
   align-items: center;
   position: relative;
+  padding: 0.5rem;
   border-top: 1px solid #eee;
 
   &:first-child {
